c1ad67b34c8eb41f6a7ce7eaae7990567cce7d7e
RAF28gitH <raf28elnooob@gmail.com>
2024-10-15 08:18:17 -0600

    Agregué comentarios para hacerlo más entendible

--- cuadraticas.cpp
+++ cuadraticas.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 struct resultado{
-    double x1,x2;
+    double x1,x2; //hice una estructura para guardar los resultados de ambas x
 };
 
 int main(){
 	double a,b,c;
-	resultado r;
+	resultado r; //creo una variable r para guardar los resultados de x1 y x2
 	cin>>a>>b>>c;
-	if(a==0){
+	if(a==0){ //compruebo que A no sea 0
 		cout<<"A no puede ser 0"<<endl;
 		return 0;
 	}
@@ -18,6 +18,6 @@ int main(){
 	}
 	r.x1=(b+sqrt(b*b-4*a*c))/(2*a);
 	r.x2=(b-sqrt(b*b-4*a*c))/(2*a);
-	cout<<r.x1<<endl<<r.x2<<endl;
+	cout<<r.x1<<endl<<r.x2<<endl; //imprimo los resultados
 	return 0;
 }
